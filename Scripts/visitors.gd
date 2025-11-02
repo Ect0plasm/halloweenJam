2ad65b5105496afd4f23333ffeed1635ea6e7ed4
Wil <98198668+Wilaz@users.noreply.github.com>
2025-11-02 18:38:47 -0500

    feat: difficulty scaling

--- Scripts/visitors.gd
+++ Scripts/visitors.gd
@@ -1,24 +1,23 @@
 extends Node
 
-@export var door: Sprite2D
-@export var mean: float
-@export var deviation: float
-@onready var visitors = [
-	preload("res://Scenes/Presets/basajaun.tscn"),
-	#preload("res://Scenes/Presets/blemmyes.tscn"),
-	#preload("res://Scenes/Presets/cyclops.tscn"),
-	preload("res://Scenes/Presets/deep.tscn"),
-	preload("res://Scenes/Presets/ogre.tscn"),
-	#preload("res://Scenes/Presets/satyr.tscn")
-]
-@export var move_speed: float
 @export var initial_position: Vector2
+
+@export_category("Nodes")
+@export var door: Sprite2D
+@export var visitors: Array[PackedScene]
 
 var timeout
 var current_visitor
 var tween: Tween
 var count = 2
 var last = count
+var visitor_count = 0
+var mean: float
+var length: int
+const deviation = 0.25
+
+func scale() -> float:
+	return max(3-(0.2*visitor_count), 1.5)
 
 func get_random() -> int:
 	var rand = randi_range(0, count-1)
@@ -33,8 +32,8 @@ func spawn_visitor():
 	current_visitor.z_index = -75
 	add_child(current_visitor)
 	tween = create_tween()
-	tween.tween_property(current_visitor, "position", door.position, move_speed)
-	await get_tree().create_timer(move_speed-0.2).timeout
+	tween.tween_property(current_visitor, "position", door.position, 0.5*scale())
+	await get_tree().create_timer(0.5*scale()-0.2).timeout
 	door.set_visible(false)
 	timeout = false
 
@@ -50,8 +49,10 @@ func button_pressed(candy: String):
 
 	door.set_visible(true)
 	remove_child(current_visitor)
+	visitor_count += 1
 	timeout = true
-	var length = clamp(randfn(mean, deviation), mean - 4 * deviation, mean + 4 * deviation)
+	mean = scale()
+	length = clamp(randfn(mean, deviation), mean - 4 * deviation, mean + 4 * deviation)
 	await get_tree().create_timer(length).timeout
 	spawn_visitor()
 
